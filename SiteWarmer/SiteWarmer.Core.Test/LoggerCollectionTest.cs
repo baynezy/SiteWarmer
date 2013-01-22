c05d6ef6c3672be00c259ed93037b7c8799e8474
Simon Baynes <baynezy@gmail.com>
2013-01-22 07:02:44 +0000

    Added LoggerCollection

---
+++ SiteWarmer/SiteWarmer.Core.Test/LoggerCollectionTest.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace SiteWarmer.Core.Test
+{
+	[TestFixture]
+	class LoggerCollectionTest
+	{
+		[Test]
+		public void Size()
+		{
+			const int expectedSize = 0;
+			var collection = new LoggerCollection();
+
+			Assert.AreEqual(expectedSize, collection.Size());
+		}
+
+		[Test]
+		public void Add()
+		{
+			const int expectedSize = 1;
+			var collection = new LoggerCollection();
+			var logger = new ConsoleLogger();
+
+			collection.Add(logger);
+
+			Assert.AreEqual(expectedSize, collection.Size());
+		}
+
+		[Test]
+		public void ImplementsILogger()
+		{
+			var collection = new LoggerCollection();
+
+			Assert.IsInstanceOf(typeof(ILogger), collection);
+		}
+
+		[Test]
+		public void Log()
+		{
+			var collection = new LoggerCollection();
+			var logger1 = new Mock<ILogger>();
+			var logger2 = new Mock<ILogger>();
+
+			logger1.Setup(m => m.Log(It.IsAny<Check>()));
+			logger2.Setup(m => m.Log(It.IsAny<Check>()));
+
+			collection.Add(logger1.Object);
+			collection.Add(logger2.Object);
+
+
+			collection.Log(new Check
+			               	{
+			               		Url = "http://www.google.com/",
+								Status = 200
+			               	});
+
+			logger1.Verify(f => f.Log(It.IsAny<Check>()), Times.Once());
+			logger2.Verify(f => f.Log(It.IsAny<Check>()), Times.Once());
+		}
+	}
+}
---
+++ SiteWarmer/SiteWarmer.Core/LoggerCollection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SiteWarmer.Core
+{
+	public class LoggerCollection : ILogger
+	{
+		private readonly IList<ILogger> _loggers;
+
+		public LoggerCollection()
+		{
+			_loggers = new List<ILogger>();
+		}
+
+		public int Size()
+		{
+			return _loggers.Count;
+		}
+
+		public void Add(ILogger logger)
+		{
+			_loggers.Add(logger);
+		}
+
+		public void Log(Check check)
+		{
+			foreach (var logger in _loggers)
+			{
+				logger.Log(check);
+			}
+		}
+	}
+}
