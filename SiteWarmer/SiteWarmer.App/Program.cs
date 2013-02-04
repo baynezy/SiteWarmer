6c23eaf5573eced5e0e0a9e764a355776c72b13d
Simon Baynes <baynezy@gmail.com>
2013-02-04 13:59:41 +0000

    Updated app to use unit testable components

--- SiteWarmer/SiteWarmer.App/Program.cs
+++ SiteWarmer/SiteWarmer.App/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using CommandLine;
-using SiteWarmer.Core;
 using SiteWarmer.Core.Comms;
 using SiteWarmer.Core.Config;
-using SiteWarmer.Core.Logging;
 
 namespace SiteWarmer.App
 {
@@ -18,9 +16,9 @@ namespace SiteWarmer.App
 			{
 				var config = new FileConfig(options.Inputfile);
 				var requester = new Requester();
-				var logger = CreateLogger(options);
+				var logger = LoggerFactory.Create(options);
 
-				var warmer = CreateWarmer(config, requester, logger, options);
+				var warmer = WarmerFactory.Create(options, config, requester, logger);
 
 				warmer.Warm();
 			}
@@ -28,31 +26,6 @@ namespace SiteWarmer.App
 			{
 				Environment.Exit(1);
 			}
-		}
-
-		private static ILogger CreateLogger(Options options)
-		{
-			ILogger logger;
-			
-			if (options.LogError)
-			{
-				var collection = new LoggerCollection();
-				collection.Add(new ConsoleLogger());
-				collection.Add(new FileLogger());
-
-				logger = collection;
-			}
-			else
-			{
-				logger = new ConsoleLogger();
-			}
-
-			return logger;
-		}
-
-		private static Warmer CreateWarmer(IConfig config, IRequester requester, ILogger logger, Options options)
-		{
-			return options.Retries > 1 ? new RepeatWarmer(config, requester, logger, options.Retries) : new Warmer(config, requester, logger);
 		}
 	}
 }
