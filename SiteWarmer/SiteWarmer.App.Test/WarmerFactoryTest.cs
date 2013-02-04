e88382d24df50cc5c9795abceac86179fea7bda3
Simon Baynes <baynezy@gmail.com>
2013-02-04 13:54:31 +0000

    Created WarmerFactory

---
+++ SiteWarmer/SiteWarmer.App.Test/WarmerFactoryTest.cs
@@ -0,0 +1,64 @@
+﻿using Moq;
+using NUnit.Framework;
+using SiteWarmer.Core;
+using SiteWarmer.Core.Comms;
+using SiteWarmer.Core.Config;
+using SiteWarmer.Core.Logging;
+
+namespace SiteWarmer.App.Test
+{
+	[TestFixture]
+	class WarmerFactoryTest
+	{
+		[Test]
+		public void CreateWarmer_WhenRetriesAreZero_ReturnWarmer()
+		{
+			var options = new Options
+				{
+					Retries = 0
+				};
+
+			var config = new Mock<IConfig>();
+			var requester = new Mock<IRequester>();
+			var logger = new Mock<ILogger>();
+
+			var warmer = WarmerFactory.CreateWarmer(options, config.Object, requester.Object, logger.Object);
+
+			Assert.IsInstanceOf(typeof(Warmer), warmer);
+		}
+
+		[Test]
+		public void CreateWarmer_WhenRetriesAreOne_ReturnWarmer()
+		{
+			var options = new Options
+			{
+				Retries = 1
+			};
+
+			var config = new Mock<IConfig>();
+			var requester = new Mock<IRequester>();
+			var logger = new Mock<ILogger>();
+
+			var warmer = WarmerFactory.CreateWarmer(options, config.Object, requester.Object, logger.Object);
+
+			Assert.IsInstanceOf(typeof(Warmer), warmer);
+		}
+
+		[Test]
+		public void CreateWarmer_WhenRetriesAreMoreThanOne_ReturnRepeatWarmer()
+		{
+			var options = new Options
+			{
+				Retries = 2
+			};
+
+			var config = new Mock<IConfig>();
+			var requester = new Mock<IRequester>();
+			var logger = new Mock<ILogger>();
+
+			var warmer = WarmerFactory.CreateWarmer(options, config.Object, requester.Object, logger.Object);
+
+			Assert.IsInstanceOf(typeof(RepeatWarmer), warmer);
+		}
+	}
+}
---
+++ SiteWarmer/SiteWarmer.App/WarmerFactory.cs
@@ -0,0 +1,15 @@
+﻿using SiteWarmer.Core;
+using SiteWarmer.Core.Comms;
+using SiteWarmer.Core.Config;
+using SiteWarmer.Core.Logging;
+
+namespace SiteWarmer.App
+{
+	public class WarmerFactory
+	{
+		public static Warmer CreateWarmer(Options options, IConfig config, IRequester requester, ILogger logger)
+		{
+			return options.Retries > 1 ? new RepeatWarmer(config, requester, logger, options.Retries) : new Warmer(config, requester, logger);
+		}
+	}
+}
