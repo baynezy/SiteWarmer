aef6b479a7e93a8ab831650f943b8b73cf914827
Simon Baynes <baynezy@gmail.com>
2013-03-02 06:26:08 +0000

    Created shared assembly info

--- SiteWarmer/SiteWarmer.Core/Properties/AssemblyInfo.cs
+++ SiteWarmer/SiteWarmer.Core/Properties/AssemblyInfo.cs
@@ -7,11 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SiteWarmer.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SiteWarmer.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f28abe61-e729-4553-b910-8604c9046a97")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.5.0")]
-[assembly: AssemblyFileVersion("3.0.5.0")]
