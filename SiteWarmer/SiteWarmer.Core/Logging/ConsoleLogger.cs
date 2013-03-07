53549cc9218f7648c0ae6dccd598262903c0bde1
Simon Baynes <baynezy@gmail.com>
2013-03-07 07:53:24 +0000

    Updated ConsoleLogger to represent errors in red

--- SiteWarmer/SiteWarmer.Core/Logging/ConsoleLogger.cs
+++ SiteWarmer/SiteWarmer.Core/Logging/ConsoleLogger.cs
@@ -5,10 +5,44 @@ namespace SiteWarmer.Core.Logging
 {
 	public class ConsoleLogger : ILogger
 	{
+		private static ConsoleColor _originalTextColour;
+		private const ConsoleColor ErrorTextColour = ConsoleColor.Red;
+
+		public ConsoleLogger()
+		{
+			_originalTextColour = Console.ForegroundColor;
+		}
+
 		public void Log(Check check)
 		{
 			var passed = (check.Passed()) ? "Passed" : "Failed";
-			Console.WriteLine(String.Format("{0}: {1} - {2}", check.Status, check.Url, passed));
+
+
+			if (check.Passed())
+			{
+				WritePass(check, passed);
+			}
+			else
+			{
+				WriteFail(check, passed);
+			}
+		}
+
+		private static void WritePass(Check check, string passed)
+		{
+			WriteToConsole(check, passed);
+		}
+
+		private static void WriteFail(Check check, string passed)
+		{
+			Console.ForegroundColor = ErrorTextColour;
+			WriteToConsole(check, passed);
+			Console.ForegroundColor = _originalTextColour;
+		}
+
+		private static void WriteToConsole(Check check, string passed)
+		{
+			Console.WriteLine("{0}: {1} - {2}", check.Status, check.Url, passed);
 		}
 
 		public void Close()
