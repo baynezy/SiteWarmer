3bbd90cce21a501c79e62f4799b1b7cf757149f2
Simon Baynes <baynezy@gmail.com>
2013-04-19 07:29:27 +0100

    parked where I am for now

---
+++ SiteWarmer/SiteWarmer.App.Test/ProgramTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+namespace SiteWarmer.App.Test
+{
+	[TestFixture]
+	class ProgramTest
+	{
+		[Test]
+		public void Main_WhenPassingOnlyLoggingArgument_ThenShouldNotThrowException()
+		{
+			var args = new[]
+					{
+						"-l"
+					};
+			AssertThatProgramNotFailing(args, "When only passing in -l there should not be an error.");
+		}
+
+		[Test]
+		public void Main_WhenPassingOnlyVersionArgument_ThenShouldNotThrowException()
+		{
+			var args = new[]
+					{
+						"--version"
+					};
+			AssertThatProgramNotFailing(args, "When only passing in --version there should not be an error.");
+		}
+
+		private static void AssertThatProgramNotFailing(string[] args, string message = "There should not be an exception here.")
+		{
+			try
+			{
+				Program.Main(args);
+			}
+			catch (Exception)
+			{
+				Assert.Fail(message);
+			}
+		}
+	}
+}
