d60de96b00d40d4f0ed194d88b693bed187cc5ba
Simon Baynes <baynezy@gmail.com>
2013-04-24 07:57:30 +0100

    Added CustomWarmer

---
+++ SiteWarmer/SiteWarmer.Core.Test/CustomWarmerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SiteWarmer.Core.Comms;
+using SiteWarmer.Core.Config;
+using SiteWarmer.Core.Logging;
+
+namespace SiteWarmer.Core.Test
+{
+	[TestFixture]
+	class CustomWarmerTest
+	{
+		[Test]
+		public void Warm_WhenProvidingDelegate_ThenRunDelegate()
+		{
+			var mockConfig = new Mock<IConfig>();
+			mockConfig.Setup(m => m.Checks).Returns(
+				new List<Check>
+				{
+					new Check
+						{
+							Url = "http://www.google.co.uk/"
+						},
+					new Check
+						{
+							Url = "http://www.github.com/"
+						}
+				});
+
+			var mockRequester = new Mock<IRequester>();
+			mockRequester.Setup(m => m.Check(It.IsAny<Check>()));
+
+			var mockLogger = new Mock<ILogger>();
+			mockLogger.Setup(m => m.Log(It.IsAny<Check>()));
+
+			var warmer = new CustomWarmer(mockConfig.Object, mockRequester.Object);
+
+			var logger = mockLogger.Object;
+
+			warmer.Warm(logger.Log);
+
+			mockRequester.Verify(f => f.Check(It.IsAny<Check>()), Times.Exactly(2));
+			mockLogger.Verify(f => f.Log(It.IsAny<Check>()), Times.Exactly(2));
+		}
+	}
+}
---
+++ SiteWarmer/SiteWarmer.Core/CustomWarmer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SiteWarmer.Core.Comms;
+using SiteWarmer.Core.Config;
+
+namespace SiteWarmer.Core
+{
+	public class CustomWarmer
+	{
+		private readonly IConfig _config;
+		private readonly IRequester _requester;
+
+		public CustomWarmer(IConfig config, IRequester requester)
+		{
+			_config = config;
+			_requester = requester;
+		}
+
+		public void Warm(Action<Check> action)
+		{
+			foreach (var check in _config.Checks)
+			{
+				_requester.Check(check);
+				action.Invoke(check);
+			}
+		}
+	}
+}
