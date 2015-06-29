c491fcec0b269470678aa6309915885d47295296
Simon Baynes <baynezy@gmail.com>
2015-06-29 12:10:19 +0100

    Updated version number

--- SiteWarmer/SharedAssemblyInfo.cs
+++ SiteWarmer/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("Simon Baynes")]
-[assembly: AssemblyCopyright("Copyright ©  2012 -2013")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // Version information for an assembly consists of the following four values:
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.2.*")]
+[assembly: AssemblyVersion("4.0.3.*")]
 //[assembly: AssemblyFileVersion("3.0.5.*")]
