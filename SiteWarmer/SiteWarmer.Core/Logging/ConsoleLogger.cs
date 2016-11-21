56ccf6c5770a72b3327c29b39a04fe8f50a0814d
Simon Baynes <baynezy@gmail.com>
2016-11-21 18:00:56 +0000

    Console now logs in Yellow when the content check fails. Closes #25

--- SiteWarmer/SiteWarmer.Core/Logging/ConsoleLogger.cs
+++ SiteWarmer/SiteWarmer.Core/Logging/ConsoleLogger.cs
@@ -11,7 +11,7 @@ namespace SiteWarmer.Core.Logging
 		private static ConsoleColor _originalTextColour;
 		private static readonly object LockObject = new object();
 		private const ConsoleColor ErrorTextColour = ConsoleColor.Red;
-		private const ConsoleColor ContentFailTextColour = ConsoleColor.DarkYellow;
+		private const ConsoleColor ContentFailTextColour = ConsoleColor.Yellow;
 		private const ConsoleColor SuccessTextColour = ConsoleColor.Green;
 
 		/// <summary>
